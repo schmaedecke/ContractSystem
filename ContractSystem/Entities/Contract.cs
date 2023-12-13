a862007291fa7c78aafb13a5848d7ca9ca419b7b
Gustavo Luis Schmaedecke <97gustavo.luis@gmail.com>
2023-12-13 09:26:34 -0300

    Primeiro serviço

--- ContractSystem/Entities/Contract.cs
+++ ContractSystem/Entities/Contract.cs
@@ -3,8 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace ContractSystem.Entities {
-    internal class Contract {
+    class Contract {
+        public  int Number { get; set; }
+        public DateTime Date { get; set; }
+        public double TotalValue { get; set; }
+
+        public List <Installment> Installments { get; set; }
+
+        public Contract() { }
+        public Contract(int number, DateTime date, double totalValue) {
+            Number = number;
+            Date = date;
+            TotalValue = totalValue;
+        }
+
+        public void AddInstallment(Installment installment) {
+            Installments.Add(installment);
+        }
+
     }
 }
--- ContractSystem/Entities/Installment.cs
+++ ContractSystem/Entities/Installment.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ContractSystem.Entities {
-    internal class Installment {
+    class Installment {
+        public DateTime DueDate { get; set; }
+        public double Amount { get; set; }
+
+        public Installment() { }
+        public Installment(DateTime dueDate, double amount) {
+            DueDate = dueDate;
+            Amount = amount;
+        }
+        public override string ToString() {
+            return DueDate.ToString("dd/MM/yyyy")
+                + " - "
+                + Amount.ToString("F2", CultureInfo.InvariantCulture);
+        }
     }
 }
